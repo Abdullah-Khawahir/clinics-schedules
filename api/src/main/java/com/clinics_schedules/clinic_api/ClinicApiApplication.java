b4416cc500b86fe5bba8d1aa4f3c2061bd1b1ae2
Abtuly <abdullah.khawahir@gmail.com>
2023-08-15 14:31:03 +0300

    changing naming of files

---
+++ api/src/main/java/com/clinics_schedules/clinic_api/ClinicApiApplication.java
@@ -0,0 +1,15 @@
+package com.clinics_schedules.clinic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicApiApplication {
+	// final Logger logger = LoggerFactory.getLogger(ClinicApiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicApiApplication.class, args);
+	}
+
+	
+}
---
+++ api/src/test/java/com/clinics_schedules/clinic_api/ClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinics_schedules.clinic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
