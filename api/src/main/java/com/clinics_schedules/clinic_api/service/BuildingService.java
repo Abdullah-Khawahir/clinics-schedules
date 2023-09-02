943f897eb773d4b1bb676842e4bf2d05cca3b723
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2023-09-02 20:27:58 +0300

    adding Building service

---
+++ api/src/main/java/com/clinics_schedules/clinic_api/service/BuildingService.java
@@ -0,0 +1,51 @@
+package com.clinics_schedules.clinic_api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clinics_schedules.clinic_api.dto.BuildingDto;
+import com.clinics_schedules.clinic_api.entity.Building;
+import com.clinics_schedules.clinic_api.exception.ResourceNotFoundException;
+import com.clinics_schedules.clinic_api.repository.BuildingRepository;
+
+@Service
+public class BuildingService {
+
+    @Autowired
+    private BuildingRepository repository;
+
+    public List<Building> getAll() {
+        return repository.findAll();
+    }
+
+    public Building save(final BuildingDto building) {
+        return repository.save(
+                Building.builder()
+                        .id(null)
+                        .arabicName(building.getArabicName())
+                        .englishName(building.getEnglishName())
+                        .number(building.getNumber())
+                        .hospitalId(building.getHospitalId())
+                        .build());
+    }
+
+    public Building updateById(final Integer id, final BuildingDto buildingDto) {
+        final Building currentBuilding = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("building", "building_id", id.toString()));
+
+        currentBuilding.setArabicName(buildingDto.getArabicName());
+        currentBuilding.setEnglishName(buildingDto.getEnglishName());
+        currentBuilding.setNumber(buildingDto.getNumber());
+
+        return repository.save(currentBuilding);
+
+    }
+
+    public void deleteById(final Integer id) throws ResourceNotFoundException {
+        if (!repository.existsById(id))
+            throw new ResourceNotFoundException("Building", "building_id", id.toString());
+        repository.deleteById(id);
+    }
+}
