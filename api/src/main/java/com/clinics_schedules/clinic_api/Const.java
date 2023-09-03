af9f1b3ef7846e97f6a2d4d62758f567b8fe6b01
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2023-09-03 19:32:46 +0300

    changing Constants to Const

---
+++ api/src/main/java/com/clinics_schedules/clinic_api/Const.java
@@ -0,0 +1,8 @@
+package com.clinics_schedules.clinic_api;
+
+public class Const {
+    public static final String SAVE_RESPONSE_MESSAGE = "entity is created",
+            VARIABLE_SIZE_VALIDATION_MESSAGE = "the size must be between 3 and 5",
+            NON_NEGATIVE_NUMBER_MESSAGE = "Only Positive Numbers and Zero are Allowed",
+            NON_BLANK_STRING_MESSAGE = "Value must not be Empty or Blank";
+}
--- api/src/main/java/com/clinics_schedules/clinic_api/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.clinics_schedules.clinic_api;
-
-public class Constants {
-    public static final String SAVE_RESPONSE_MESSAGE ="entity is created";
-}
