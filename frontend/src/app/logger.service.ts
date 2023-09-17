5f552e2c60e89fb0d4828c6b2f1332ee9a4a038a
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2023-09-17 17:02:14 +0300

    fix logger from targeting cmd to browser console

--- frontend/src/app/logger.service.ts
+++ frontend/src/app/logger.service.ts
@@ -1,42 +1,15 @@
 import { Injectable } from '@angular/core';
 
 
-type LogObject = { data: string, type: LogType }
-const Colors = {
-  Reset: "\x1b[0m"
-  , Bright: "\x1b[1m"
-  , Dim: "\x1b[2m"
-  , Underscore: "\x1b[4m"
-  , Blink: "\x1b[5m"
-  , Reverse: "\x1b[7m"
-  , Hidden: "\x1b[8m"
+interface LogObject { data: string, type: LogType }
 
-  , FgBlack: "\x1b[30m"
-  , FgRed: "\x1b[31m"
-  , FgGreen: "\x1b[32m"
-  , FgYellow: "\x1b[33m"
-  , FgBlue: "\x1b[34m"
-  , FgMagenta: "\x1b[35m"
-  , FgCyan: "\x1b[36m"
-  , FgWhite: "\x1b[37m"
-  , FgGray: "\x1b[90m"
-
-  , BgBlack: "\x1b[40m"
-  , BgRed: "\x1b[41m"
-  , BgGreen: "\x1b[42m"
-  , BgYellow: "\x1b[43m"
-  , BgBlue: "\x1b[44m"
-  , BgMagenta: "\x1b[45m"
-  , BgCyan: "\x1b[46m"
-  , BgWhite: "\x1b[47m"
-  , BgGray: "\x1b[100m"
-}
 type LogType = "Error" | "Warning" | "INFO" | "Debug"
+
 @Injectable({
   providedIn: 'root'
 })
 export default class Logger {
-  private static MAX_SIZE: number = 1200;
+  private static MAX_SIZE: number = 120;
   private static logsBuffer: Array<LogObject> = []
   private static isDebug: boolean = true;
 
@@ -58,20 +31,18 @@ export default class Logger {
 
 
     if (type == undefined || type == "INFO") {
-      const logString = `[INFO]\t` + data.toString();
+      const logString = `[INFO]\t` + JSON.stringify(data);
       console.log(`${logString}`);
       this.logsBuffer.push({ data: logString, type: "INFO" })
     }
     if (type === "Error") {
-      const color = Colors.FgRed
-      const logString = `[ERROR]\t` + data.toString();
-      console.error(`${color}${logString}${Colors.Reset}`);
+      const logString = `[ERROR]\t` + JSON.stringify(data);
+      console.error(logString);
       this.logsBuffer.push({ data: logString, type: "Error" })
     }
     if (type === "Warning") {
-      const color = Colors.FgYellow
-      const logString = "[WARNING]\t" + data.toString();
-      console.warn(`${color}${logString}${Colors.Reset}`);
+      const logString = "[WARNING]\t" + JSON.stringify(data);
+      console.warn(logString);
       this.logsBuffer.push({ data: logString, type: "Warning" })
     }
     if (type == "Debug") {
@@ -82,12 +53,10 @@ export default class Logger {
       this.logsBuffer.shift();
     }
 
-
-
   }
   static debugLog(data: Object) {
     if (this.isDebug)
-      console.debug(`${Colors.FgCyan}[DEBUG]\t${data}${Colors.Reset}`)
+      console.debug(`$[DEBUG]\t${JSON.stringify(data)}`)
   }
   static lastLog() {
     return this.logsBuffer.at(-1);
